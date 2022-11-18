10cbd42df568426df1544c33380137ce5a1c413d
freddie316 <freddiewild@hawaii.rr.com>
2022-11-17 17:45:03 -0700

    Update bokehTest.py

--- src/bokehTest.py
+++ src/bokehTest.py
@@ -8,7 +8,7 @@ Created on Thu Nov 17 12:41:13 2022
 import os
 from numpy import pi,sin,linspace
 from pathlib import Path
-from bokeh.plotting import figure, output_file, save, show
+from bokeh.plotting import figure, output_file, show
 
 plot = figure(title = "Sine Wave example", x_axis_label='x', y_axis_label='y')
 x = linspace(0,2*pi,1000)
