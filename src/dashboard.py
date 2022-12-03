f4e966613a1aaf67568cf41df5932836bdcd3eb4
freddie316 <freddiewild@hawaii.rr.com>
2022-12-02 21:46:36 -0700

    Modified dashboard.py, made a new country list for WHO
    Modified the dashboard.py file to ask for the website first, then open the correct country list file. Also added a rudimentary similarity checker for mis entered countries based on the list.

--- src/dashboard.py
+++ src/dashboard.py
@@ -10,27 +10,41 @@ class MyException(Exception):
     pass
 
 
-def get_user_input(n=5):
-    with open('country-list.txt') as country_list:
-        data = country_list.read()
-        valid_countries = data.split('\n')
-        print(valid_countries)
-
+def get_user_input(n=5):    
+    while n>0:
+        site = input("Choose a website to search on: WorldOMeter, WHO:\t")
+        if site.lower() == 'worldometer' or site.lower() == 'wom':
+            with open('country-list-WOM.txt') as country_list:
+                data = country_list.read()
+                valid_countries = data.split('\n')
+            break
+        elif site.lower() == 'who' or site.lower() == 'world health organization':
+            with open('country-list-WHO.txt') as country_list:
+                data = country_list.read()
+                valid_countries = data.split('\n')
+            break
+        else:
+            print("Invalid Website Selection, valid selections are 'WorldOMeter', 'WOM', 'WHO', or 'World Health Organization'")
+            n-=1
+            print(f"{n} attempts remaining")
+            continue
+    
+    n = 5
     while n>0:
         country = input("Input a country to search for:\t")
         if country not in valid_countries:
             print("Invalid Country Selection")
+            print('Similar Valid Countries are:')
+            for i in valid_countries:
+                if country[0] == i[0]:
+                    print(i)
             n-=1
             print(f"{n} attempts remaining")
             continue
-        site = input("Choose a website to search on: WorldOMeter, WHO:\t")
-        if site.lower() == 'worldometer' or site.lower() == 'who' or site.lower() == 'world health organization':
-            return country, site
         else:
-            print("Invalid Website Selection, valid selections are 'WorldOMeter', 'WHO', or 'World Health Organization'")
-            n-=1
-            print(f"{n} attempts remaining")
-            continue
+            break
+
+    return country, site    
     valid_countries.close()
     raise MyException('Maximum attempts reached!')
 
